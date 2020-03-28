5528b888cfa47cef0f3730735567baa9b63fb93b
OskaMathis <oskamathis@gmail.com>
2020-03-28 14:56:25 +0900

    modify endpoint to get task

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,9 @@
-module.exports.tasks = require('./routes/tasks').show;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const functions = require('firebase-functions');
+
+app.use(cors({ origin: true }));
+app.use('/tasks', require('./routes/tasks.js'));
+
+module.exports.api = functions.region('asia-northeast1').https.onRequest(app);
--- functions/routes/tasks.js
+++ functions/routes/tasks.js
@@ -1,25 +1,29 @@
-const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
 const admin = require('firebase-admin');
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
-    databaseURL: "https://next-todo-002.firebaseio.com"
+    databaseURL: 'https://next-todo-002.firebaseio.com'
 });
 const db = admin.firestore();
+const router = require('express-promise-router')();
 
+/**
+ * タスク取得
+ */
+router.get('/:id', async (req, res) => {
+    let ref = db.collection('tasks').doc(req.params.id);
+    let task = await ref.get().catch(err => console.log('Error getting document', err));
 
-module.exports.show = functions.region('asia-northeast1').https.onRequest((req, res) => {
-    cors(req, res, async () => {
-        let ref = db.collection('tasks').doc('aJDm3esbPPlGLKseXPXp');
-        let task = await ref.get().catch(err => console.log('Error getting document', err));
+    if (!task.exists) {
+        console.log('No such document!');
+        res.status(404).send({ message: 'Not Found' });
+    } else {
+        console.log('Document data:', task.data());
+        res.send(task.data());
+    };
+});
 
-        if (!task.exists) {
-            console.log('No such document!');
-            res.status(404).send({ "message": "No such document!" });
-        } else {
-            console.log('Document data:', task.data());
-            res.send(task.data());
-        };
     });
 });
 
+
+module.exports = router
