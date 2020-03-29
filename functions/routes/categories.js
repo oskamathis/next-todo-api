79a7a3c51e0fe9f78c033ee060ff4cc04a4b62b9
OskaMathis <oskamathis@gmail.com>
2020-03-29 20:38:46 +0900

    add/modify endpoint to manage categories

--- functions/routes/categories.js
+++ functions/routes/categories.js
@@ -7,16 +7,50 @@ const auth = require('./auth.js');
  * カテゴリ登録
  */
 router.post('/', async (req, res) => {
-    // let userRef = auth.getUserRef(req);
     let userId = auth.getUserId(req);
-    let userRef = await db.collection('users').doc(userId);
 
     let addCategory = {
         'name': req.body.name
     }
 
-    await db.collection('categories').add({ 'user': userRef, ...addCategory })
+    await db.collection('categories').add({ 'user_id': userId, ...addCategory })
     res.send(addCategory);
 });
+
+/**
+ * カテゴリ一覧取得
+ */
+router.get('/', async (req, res) => {
+    let userId = auth.getUserId(req);
+    let categories = [];
+    await db.collection('categories').where('user_id', '==', userId).get().then(snapshot => {
+        if (snapshot.empty) {
+            console.log('No matching documents.');
+            return;
+        }
+
+        snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            categories.push({
+                'id': doc.id,
+                'name': doc.data().name
+            });
+        })
+    });
+
+    res.send(categories);
+});
+
+/**
+ * カテゴリ削除
+ */
+router.delete('/:id', async (req, res) => {
+    await db.collection('categories').doc(req.params.id).delete();
+
+    res.send({
+        'message': 'Successfully deleted category.'
+    });
+});
+
 
 module.exports = router;
